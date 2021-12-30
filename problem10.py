9d92089f412cba454bc0583069d4254614656a32
Muhammad Ahsan <69692532+Muhammad-Ahsan-Rasheed@users.noreply.github.com>
2021-12-30 14:20:38 +0500

    Add files via upload

--- problem10.py
+++ problem10.py
@@ -1,17 +1,12 @@
 def SieveOfEratosthenes(n):
-
     prime = [True for i in range(n+1)]
     p = 2
-    while (p * p <= n):
-        if (prime[p] == True):
-
+    while p * p <= n:
+        if prime[p] == True:
             for i in range(p * p, n+1, p):
                 prime[i] = False
         p += 1
-    pri = []
-    for p in range(2, n+1):
-        if prime[p]:
-            pri.append(p)
+    pri = [p for p in range(2, n+1) if prime[p] ]
     return pri
 
 if __name__ == '__main__':
